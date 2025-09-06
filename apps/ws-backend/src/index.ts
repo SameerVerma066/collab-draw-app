44c79c1804488b2e6980d7407afd5810257dc4bc
SameerVerma066@ <SameerVerma066@>
2025-09-06 19:55:41 +0000

    ws layer implemented

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,37 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 const wss = new WebSocketServer({ port: 8080 });
+//state-management using global variable 
+interface User {
+  ws: WebSocket;
+  rooms: string[];
+  userId: string;
+}
+
+const users: User[] = [];
+
+
+function checkUser (token:string):string | null{
+  try{
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+  if(typeof decoded == "string") {
+    return null;
+  }
+
+  if(!decoded || !decoded.userId) {
+    
+    return null;
+  }
+  return decoded.userId;
+
+  }catch(e)
+  {
+    return null;
+  }
+  return null;
+}
 
 wss.on('connection', function connection(ws, request) {
   const url = request.url;
@@ -12,16 +42,46 @@ wss.on('connection', function connection(ws, request) {
   const queryParams = new URLSearchParams(url.split ('?')[1]);
   const token = queryParams.get('token')|| "";
 
-  const decoded = jwt.verify(token, JWT_SECRET);
+  const userId = checkUser(token);
 
-  if(!decoded || !(decoded as JwtPayload).userId){
-      ws.close;
-      return;
-
+  if(!userId == null){
+    ws.close;
+    return null;
   }
 
+
+
   ws.on('message', function message(data) {
-      ws.send('pong');
+      const parsedData = JSON.parse(data as unknown as string);
+      
+      if (parsedData.type === "join_room") {
+        const user = users.find((x) => x.ws === ws);
+        user?.rooms.push(parsedData.roomId);
+      }
+
+      if (parsedData.type === "leave_room") {
+        const user = users.find((x) => x.ws === ws);
+        if (!user) {
+          return;
+        }
+        user.rooms = user?.rooms.filter((x) => x === parsedData.room);
+      }
+
+      if (parsedData.type === "chat") {
+        //TODO: check if the message isn't too long; also check if the message doesnot have something abnoxious written in it
+  
+        const roomId = parsedData.roomId;
+        const message = parsedData.message;
+
+        users.forEach(user => {
+          if (user.rooms.includes(roomId)){
+            user.ws.send(JSON.stringify({
+              type :"chat",
+              message: message,
+              roomId
+            }))};
+        })
+      }
   });
 
 });
