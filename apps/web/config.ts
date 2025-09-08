77e1e374606be3f098c3420f1b6facb1fed75cc1
SameerVerma066@ <SameerVerma066@>
2025-09-08 15:14:58 +0000

    first shape done

---
+++ apps/web/config.ts
@@ -0,0 +1,5 @@
+
+export const BACKEND_URL= "http://localhost:3001";
+export const WS_URL= "ws://localhost:8080";
+
+// export const storedToken = localStorage.getItem("jwtToken"); //trying on my own to dynamically get the token value fromt he local storage
