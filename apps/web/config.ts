f1655019eb7767dbb08e77eff78e0f238c884bed
SameerVerma066@ <SameerVerma066@>
2025-10-04 09:51:35 +0000

    implemented the workflows

--- apps/web/config.ts
+++
@@ -1,5 +0,0 @@
-
-export const BACKEND_URL= "http://localhost:3004";
-export const WS_URL= "ws://localhost:8080";
-
-// export const storedToken = localStorage.getItem("jwtToken"); //trying on my own to dynamically get the token value fromt he local storage
